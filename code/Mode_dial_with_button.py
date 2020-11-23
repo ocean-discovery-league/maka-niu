acd74ad2a2bf3f9c2fba95c9e66a8580466116ff
Lui Kawasumi <kawasumilui@gmail.com>
2020-11-23 16:13:27 +0000

    bit more fine tuning on haptic feedback. The clicks are nicer, but too hard notice by feel. Hearing them gives the illusion that they are enough, but with noise canceling headphones on, I can barely tell it's happening. Replaced with short ramp down buzzes.

--- code/Mode_dial_with_button.py
+++ code/Mode_dial_with_button.py
@@ -18,7 +18,7 @@ drv = adafruit_drv2605.DRV2605(i2c)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(12, GPIO.OUT)
 GPIO.setup(13, GPIO.OUT)
-red = GPIO.PWM(12, 100) #(pin, freq)
+red = GPIO.PWM(12, 1000) #(pin, freq)
 #green = GPIO.PWM(13, 100) #(pin, freq)
 #//green_duty_cycle = 1.0 # for fading pulse
 #//green_pulse_up = 1
@@ -90,6 +90,7 @@ while True:
    if (hall_mode == 1):
       if (hall_mode != hall_mode_last):
          print('Wifi Mode actvated. three red flashes')
+         sys.stdout.flush()
          drv.sequence[0] = adafruit_drv2605.Effect(58) #58 is solif buzz
          drv.play()
          for x in range(3):
@@ -103,6 +104,7 @@ while True:
    if (hall_mode == 2):
       if (hall_mode != hall_mode_last):
          print('Video Mode activated, press button to begin recording')
+         sys.stdout.flush()
          drv.sequence[0] = adafruit_drv2605.Effect(58) #58, 64, 95 are good choice transition buzzes
          drv.play()
          red.start(100)
@@ -112,7 +114,9 @@ while True:
          if (hall_button_active and recording == 0):
             #BEGIN RECORDING
             print('Starting video capture now')
-            drv.sequence[0] = adafruit_drv2605.Effect(1)
+            sys.stdout.flush()
+            drv.stop()
+            drv.sequence[0] = adafruit_drv2605.Effect(74) #1
             drv.play()
             red.stop()
             recording = 1
@@ -120,7 +124,9 @@ while True:
          elif (hall_button_active and recording):
             #END RECORDING
             print('Ending video capture')
-            drv.sequence[0] = adafruit_drv2605.Effect(10)
+            sys.stdout.flush()
+            drv.stop()
+            drv.sequence[0] = adafruit_drv2605.Effect(74) #10 nice double
             drv.play()
             red.start(100)
             recording = 0
@@ -128,6 +134,7 @@ while True:
       if (end_recording_mode_changed_flag):
          #END RECORDING
          print('Left Camera mode, ending video capture')
+         sys.stdout.flush()
          recording = 0
          end_recording_mode_changed_flag = 0
 
@@ -138,6 +145,7 @@ while True:
    if (hall_mode == 3):
       if (hall_mode != hall_mode_last):
          print('Picture Mode activated, 5 flashes. Press button to capture image, hold for burst')
+         sys.stdout.flush()
          drv.sequence[0] = adafruit_drv2605.Effect(58)
          drv.play()
          for x in range (5): 
@@ -147,17 +155,24 @@ while True:
             red.stop()
       #Flash red both only for press
       if (hall_button_active and hall_button_active != hall_button_last):
-         drv.sequence[0] = adafruit_drv2605.Effect(17)
+         drv.stop()
+         drv.sequence[0] = adafruit_drv2605.Effect(74)
          drv.play()
          red.start(100)
          sleep(0.1)
          red.stop()
          sleep(0.15)
          #CAPTURE PHOTO
+         print('Photo capture')
+         sys.stdout.flush()
+
       elif (hall_button_active):
-         drv.sequence[0] = adafruit_drv2605.Effect(11)
+         drv.stop()
+         drv.sequence[0] = adafruit_drv2605.Effect(80) #17 for solid click
          drv.play()
          #CAPTURE BURST
+         print('*')
+         sys.stdout.flush()
          sleep(0.25)
 
 
@@ -165,6 +180,7 @@ while True:
 #Magnet at Mission 1, one long red flash
    if (hall_mode == 4 and hall_mode != hall_mode_last):
       print('Mission 1 activated')
+      sys.stdout.flush()
       drv.sequence[0] = adafruit_drv2605.Effect(58)
       drv.play()
       red.start(100)
@@ -174,6 +190,7 @@ while True:
 #Magnet at Mission 2 , flash  red then green twice.
    if (hall_mode == 5 and hall_mode != hall_mode_last):
       print('Mission 2 activated')
+      sys.stdout.flush()
       drv.sequence[0] = adafruit_drv2605.Effect(58)
       drv.play()
       red.start(100)
@@ -188,18 +205,19 @@ while True:
 #Magnet at Power Off Hall,  flash red long, medium, short.
    if (hall_mode == 6 and hall_mode != hall_mode_last):
       print('Powerdown activated')
+      sys.stdout.flush()
       drv.sequence[0] = adafruit_drv2605.Effect(47)
       drv.play()
       red.stop()
-      for x in range (3): #3 short then long flashes
+      for x in range (2): #2 short then long flashes
          red.start(100)
-         sleep(0.16)
+         sleep(0.1)
          red.stop()
-         sleep(0.25)
+         sleep(0.2)
          red.start(100)
-         sleep(0.5)
+         sleep(0.4)
          red.stop()
-         sleep(0.25)
+         sleep(0.2)
       GPIO.cleanup()
       call("sudo shutdown -h now", shell=True)
       sys.exit()
