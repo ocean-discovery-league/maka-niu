902e969bde14ec9e7225bc4672cb8e775dac629a
Lui Kawasumi <kawasumilui@gmail.com>
2021-01-15 22:36:46 +0000

    commented out drv vibration code because it causes crash if device not present

--- code/MakaNiu.py
+++ code/MakaNiu.py
@@ -14,16 +14,21 @@ import qwiic_titan_gps
 
 ############################################################### SETUP
 #setup i2c for haptic feedback
-i2c = busio.I2C(board.SCL, board.SDA)
-drv = adafruit_drv2605.DRV2605(i2c)
+#i2c = busio.I2C(board.SCL, board.SDA)
+#drv = adafruit_drv2605.DRV2605(i2c)
 
 #setup i2c for GPS, would really like to fuse all the i2c into one library
 gps = qwiic_titan_gps.QwiicTitanGps()
+gps_available = True
 if gps.connected is False:
+   gps_available = False
    print("Could not connect to GPS.", file = sys.stderr)
    gps.begin()
 t = time.time()
 have_gps_fix = False
+
+#setup timer for videos (30 minute videos deal with power loss)
+video_started_time = time.time()
 
 #setup LED pwms pins
 GPIO.setmode(GPIO.BCM)
@@ -61,13 +66,14 @@ while True:
 
 
 # collect GPS data if desired, in X second (no less than 1) intervals
-   if gps.connected is True and (time.time() - t) > 5:
-      t = time.time()
-      if gps.get_nmea_data() is True:
-         for k, v in gps.gnss_messages.items():
-            print(k, ":", v)
-         print('\n')
-         sys.stdout.flush()
+   if gps_available:
+      if gps.connected is True and (time.time() - t) > 5:
+         t = time.time()
+         if gps.get_nmea_data() is True:
+            for k, v in gps.gnss_messages.items():
+               print(k, ":", v)
+            print('\n')
+            sys.stdout.flush()
 
 # collect raw hall states, use 1 - GPIO, because they are pull up and active low.
    hall_button_last = hall_button_active
@@ -107,24 +113,26 @@ while True:
 # Magnet at Wifi Mode,  three red flashes. 
    if (hall_mode == 1):
       if (hall_mode != hall_mode_last):
+         os.system('sudo ifconfig wlan0 up')
          print('Wifi Mode actvated. three red flashes')
          sys.stdout.flush()
-         drv.sequence[0] = adafruit_drv2605.Effect(58) #58 is solif buzz
-         drv.play()
+#         drv.sequence[0] = adafruit_drv2605.Effect(58) #58 is solif buzz
+#         drv.play()
          for x in range(3):
             sleep(0.2)
             red.start(100)
             sleep(0.2)
             red.stop()
-         drv.stop()
+#         drv.stop()
 
 #Magnet at Video Mode, constant red one. When button pressed, go dark and record video. Press again, end video and start led
    if (hall_mode == 2):
       if (hall_mode != hall_mode_last):
+         #os.system('sudo ifconfig wlan0 down')
          print('Video Mode activated, press button to begin recording')
          sys.stdout.flush()
-         drv.sequence[0] = adafruit_drv2605.Effect(58) #58, 64, 95 are good choice transition buzzes
-         drv.play()
+#         drv.sequence[0] = adafruit_drv2605.Effect(58) #58, 64, 95 are good choice transition buzzes
+#         drv.play()
          red.start(100)
          recording = 0
 
@@ -134,20 +142,21 @@ while True:
             os.system('echo ca 1 > /var/www/html/FIFO')
             print('Starting video capture now')
             sys.stdout.flush()
-            drv.stop()
-            drv.sequence[0] = adafruit_drv2605.Effect(74) #1
-            drv.play()
+#            drv.stop()
+#            drv.sequence[0] = adafruit_drv2605.Effect(74) #1
+#            drv.play()
             red.stop()
             recording = 1
+            video_started_time = time.time()
 
          elif (hall_button_active and recording):
             #END RECORDING
             os.system('echo ca 0 > /var/www/html/FIFO')
             print('Ending video capture')
             sys.stdout.flush()
-            drv.stop()
-            drv.sequence[0] = adafruit_drv2605.Effect(74) #10 nice double
-            drv.play()
+#            drv.stop()
+#            drv.sequence[0] = adafruit_drv2605.Effect(74) #10 nice double
+#            drv.play()
             red.start(100)
             recording = 0
 
@@ -158,15 +167,24 @@ while True:
          recording = 0
          end_recording_mode_changed_flag = 0
 
+      if (recording and (time.time()-video_started_time) > 899):
+         os.system('echo ca 0 > /var/www/html/FIFO')
+         print('Video capture time limit reached: Start new capture')
+         sys.stdout.flush()
+         sleep(0.5)
+         os.system('echo ca 1 > /var/www/html/FIFO')
+         sys.stdout.flush()
+         video_started_time = time.time()
 
 
 #Magnet at Picture Mode, 5 red flashes.
    if (hall_mode == 3):
       if (hall_mode != hall_mode_last):
+         #os.system('sudo ifconfig wlan0 down')
          print('Picture Mode activated, 5 flashes. Press button to capture image, hold for burst')
          sys.stdout.flush()
-         drv.sequence[0] = adafruit_drv2605.Effect(58)
-         drv.play()
+#         drv.sequence[0] = adafruit_drv2605.Effect(58)
+#         drv.play()
          for x in range (5): 
             sleep(0.12)
             red.start(100)
@@ -174,9 +192,9 @@ while True:
             red.stop()
       #Flash red both only for press
       if (hall_button_active and hall_button_active != hall_button_last):
-         drv.stop()
-         drv.sequence[0] = adafruit_drv2605.Effect(74)
-         drv.play()
+#         drv.stop()
+#         drv.sequence[0] = adafruit_drv2605.Effect(74)
+#         drv.play()
          red.start(100)
          sleep(0.1)
          red.stop()
@@ -187,9 +205,9 @@ while True:
          sys.stdout.flush()
 
       elif (hall_button_active):
-         drv.stop()
-         drv.sequence[0] = adafruit_drv2605.Effect(80) #17 for solid click
-         drv.play()
+#         drv.stop()
+#         drv.sequence[0] = adafruit_drv2605.Effect(80) #17 for solid click
+#         drv.play()
          #CAPTURE BURST
          os.system('echo im 1 > /var/www/html/FIFO')
          print('*')
@@ -200,20 +218,22 @@ while True:
 
 #Magnet at Mission 1, one long red flash
    if (hall_mode == 4 and hall_mode != hall_mode_last):
+      os.system('sudo ifconfig wlan0 down')
       print('Mission 1 activated')
       sys.stdout.flush()
-      drv.sequence[0] = adafruit_drv2605.Effect(58)
-      drv.play()
+#      drv.sequence[0] = adafruit_drv2605.Effect(58)
+#      drv.play()
       red.start(100)
       sleep(0.75)
       red.stop()
 
 #Magnet at Mission 2 , flash  red then green twice.
    if (hall_mode == 5 and hall_mode != hall_mode_last):
+      os.system('sudo ifconfig wlan0 down')
       print('Mission 2 activated')
       sys.stdout.flush()
-      drv.sequence[0] = adafruit_drv2605.Effect(58)
-      drv.play()
+#      drv.sequence[0] = adafruit_drv2605.Effect(58)
+#      drv.play()
       red.start(100)
       sleep(0.75)
       red.stop()
@@ -227,8 +247,8 @@ while True:
    if (hall_mode == 6 and hall_mode != hall_mode_last):
       print('Powerdown activated')
       sys.stdout.flush()
-      drv.sequence[0] = adafruit_drv2605.Effect(47)
-      drv.play()
+#      drv.sequence[0] = adafruit_drv2605.Effect(47)
+#      drv.play()
       red.stop()
       for x in range (2): #2 short then long flashes
          red.start(100)
